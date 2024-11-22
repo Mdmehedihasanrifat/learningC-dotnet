1fa4a681b9f993aa94f81aba21988dd9fa20a51c
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2024-11-22 15:07:14 +0600

    variable

--- firstconsoleapp/Program.cs
+++ firstconsoleapp/Program.cs
@@ -3,5 +3,18 @@
 
     public static void Main(string[] args){
         System.Console.WriteLine("Hello World");
+        // varibale declaration
+        int a = 10;
+        int b = 20;
+        double c = 10.5;
+        float e = 10.5f;
+        string d= "Hello";
+        bool f = true;
+
+        Console.WriteLine($"{a}, {b}, {c}, {d}, {e}, {f}");
     }
+
+
+    
+
 }
